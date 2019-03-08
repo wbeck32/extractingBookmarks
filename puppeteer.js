bfa753970723004d5e3de9b4efe5e2d3481bc2b1
Your Name <you@example.com>
2019-03-07 20:49:31 -0800

    need to ease traffic load

--- puppeteer.js
+++ puppeteer.js
@@ -1,18 +1,25 @@
 const HCCrawler = require('headless-chrome-crawler');
 
-const urls = require('./deduped-small');
+const urls = require('./deduped');
 const urlsToCheck = urls.split(',');
+let validLinks = [];
 
 
 (async () => {
   const crawler = await HCCrawler.launch({
-    // Function to be called with evaluated results from browsers
+    evaluatePage: (() => ({
+      title: $('title').text(),
+    })),
     onSuccess: (result => {
-      console.log({ url: result.response.url, status: result.response.status });
-      // if (result.response.status === 200) successfulLinks.push(result.response.url)
+      if (result.response.status === 200) {
+        console.log({ url: result.response.url, title: result.result.title, status: result.response.status });
+        const urlToSave = { url: result.response.url, title: result.result.title, status: result.response.status };
+        validLinks.push(urlToSave);
+      }
     })
   });
   await crawler.queue(urlsToCheck);
   await crawler.onIdle(); // Resolved when no queue is left
   await crawler.close(); // Close the crawler
+  console.log(50, validLinks);
 })();
