8b5d6b14069b829c76719f9d1d1e93bed188936c
Your Name <you@example.com>
2019-03-09 19:29:12 -0800

    switching to Apify and starting to extract from HTML files

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # dedupingBookmarks
 
 This is a script I wrote to extract bookmarks from an exported HTML bookmarks file, dedupe them and then write a puppeteer test to eliminate sites that return a status of 404.
+
+# TODOS
+- instead of requiring a downloaded HTML file, go directly to a URL
+- remove(?) query strings and header/footer/etc. specific to the domain URLs
+- limit number of concurrent connections to avoid crippling machine performance
+- save URLs and titles for URLs that return a status of 200
+- generate a new bookmarks.html file and store it in the right place
+- remove folders?
+- how to organize?
+- sort?
--- puppeteer.js
+++ puppeteer.js
@@ -1,25 +1,49 @@
-const HCCrawler = require('headless-chrome-crawler');
-
+// const HCCrawler = require('headless-chrome-crawler');
+const Apify = require('apify');
 const urls = require('./deduped');
 const urlsToCheck = urls.split(',');
 let validLinks = [];
 
 
-(async () => {
-  const crawler = await HCCrawler.launch({
-    evaluatePage: (() => ({
-      title: $('title').text(),
-    })),
-    onSuccess: (result => {
-      if (result.response.status === 200) {
-        console.log({ url: result.response.url, title: result.result.title, status: result.response.status });
-        const urlToSave = { url: result.response.url, title: result.result.title, status: result.response.status };
-        validLinks.push(urlToSave);
-      }
-    })
+Apify.main(async () => {
+  const requestQueue = await Apify.openRequestQueue();
+  await requestQueue.addRequest({ url: 'https://www.iana.org/' });
+  const pseudoUrls = [new Apify.PseudoUrl('https://www.iana.org/[.*]')];
+
+  const crawler = new Apify.PuppeteerCrawler({
+    requestQueue,
+    handlePageFunction: async ({ request, page }) => {
+      const title = await page.title();
+      console.log(`Title of ${request.url}: ${title}`);
+      await Apify.utils.puppeteer.enqueueLinks({ page, selector: 'a', pseudoUrls, requestQueue });
+    },
+    maxRequestsPerCrawl: 100,
+    maxConcurrency: 10,
   });
-  await crawler.queue(urlsToCheck);
-  await crawler.onIdle(); // Resolved when no queue is left
-  await crawler.close(); // Close the crawler
-  console.log(50, validLinks);
-})();
+
+  await crawler.run();
+});
+
+
+
+
+// (async () => {
+//   const crawler = await HCCrawler.launch({
+//     evaluatePage: (() => {
+//       return ({
+//         title: $('title').text(),
+//       });
+//     }),
+//     onSuccess: (result => {
+//       if (result.response.status === 200) {
+//         console.log({ url: result.response.url, title: result.result.title, status: result.response.status });
+//         const urlToSave = { url: result.response.url, title: result.result.title, status: result.response.status };
+//         validLinks.push(urlToSave);
+//       }
+//     })
+//   });
+//   await crawler.queue(urlsToCheck);
+//   await crawler.onIdle(); // Resolved when no queue is left
+//   await crawler.close(); // Close the crawler
+//   console.log(50, validLinks);
+// })();
